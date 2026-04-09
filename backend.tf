347369efc74209ef2e3fd252117b742fc5e1e3e8
seyi <seyithenerd25@gmail.com>
2026-04-09 13:03:58 -0500

    removed real bucket name

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraformstate-tay"
+    bucket = "replace here with your bucket name"
     key    = "terraform.tfstate"
     region = "us-east-2" # fix: change from us-east-1 to us-east-2
   }
